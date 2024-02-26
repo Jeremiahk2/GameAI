6e08e4126fed57bc325828cecaa8196be131b7f2
Jeremiahk2 <TheMainMoe@gmail.com>
2024-02-26 10:14:50 -0800

    Changed folder names

--- HW2/P3/main.cpp
+++ HW2/P3/main.cpp
@@ -7,110 +7,14 @@
 #include <SFML/OpenGL.hpp>
 #include <SFML/Graphics.hpp>
 
-//Breadcrumb class
-class crumb : sf::CircleShape
-{
-    public:
-        crumb(int id)
-        {
-            //set initial position and size breadcrumbs   
-            this->id = id;         
-            this->setRadius(10.f);
-            this->setOrigin(10.f, 10.f);
-            this->setFillColor(sf::Color::Red);
-            this->setPosition(-100, -100);
-        }
-
-        //tell breadcrumb to render self, using current render window
-        void draw(sf::RenderWindow* window)
-        {
-            window->draw(*this);
-        }
-
-        //set position of breadcrumb
-        void drop(float x, float y)
-        {
-            this->setPosition(x, y);
-        }
-
-        //set position of breadcrumb
-        void drop(sf::Vector2f position)
-        {
-            this->setPosition(position);
-        }
-
-    private:
-        int id;
-};
-
-class boid
-{
-    public:
-        boid(sf::RenderWindow* w, sf::Texture& tex, std::vector<crumb>* crumbs)
-        {
-            window = w;
-            drop_timer = 15.f;
-            crumb_idx = 0;
-            sprite.setScale(0.05f, 0.05f);
-            sprite.setOrigin(sf::Vector2f(17.0/ .05f, 17.0 / .05f));
-            sprite.setPosition(sf::Vector2f(200.f, 200.f));          
-            sprite.setTexture(tex);
-            sprite.setScale(0.05f, 0.05f);
-            breadcrumbs = crumbs;
-        }
-
-        void draw()
-        {            
-            window->draw(sprite);
-        }  
-
-        void move()
-        {
-            //basic timer for leaving breadcrumbs
-            if (drop_timer > 0)
-            {
-                drop_timer -= 0.1f;
-            }
-            else
-            {
-                drop_timer = 15.f;
-                breadcrumbs->at(crumb_idx).drop(sprite.getPosition());
-
-                if (crumb_idx < 9)
-                    crumb_idx++;
-                else
-                    crumb_idx = 0;
-            }
-        }
-
-        void setPosition (sf::Vector2f newPos) {
-            sprite.setPosition(newPos);
-        }
-
-        sf::Vector2f getPosition() {
-            return sprite.getPosition();
-        }
-
-        void setRotation(float rotation) {
-            sprite.setRotation(rotation);
-        }
-
-    private:
-        //indice variables
-        int crumb_idx;
-        
-        //float variables
-        float drop_timer;
-        
-        //renderable objects
-        sf::Sprite sprite;
-        sf::RenderWindow* window;    
-
-        //point of breadcrumbs
-        std::vector<crumb>* breadcrumbs;
-};
+//Notes: Center of mass = X1 + X2 + X3... / M1 + M2 + M3...
+//In our case that means that the X coordinate for the center of mass is the sum of all the X coordinates divided by the number of sprites. The average.
+//Same with the Y coordinate.
+// "Neighborhood" of a boid can just be a simple radius. Reynolds suggests making it a cone but fuck him
 
 int main() {
+
+    srand(time(0));
     // Create a window with the same pixel depth as the desktop, with 144 frames per second.
     sf::RenderWindow window;
     sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
@@ -125,31 +29,29 @@ int main() {
         // error...
     }
 
-    //Set up bread crumbs
-    std::vector<crumb> breadcrumbs = std::vector<crumb>();
-    for(int i = 0; i < 10; i++)
-    {
-        crumb c(i);
-        breadcrumbs.push_back(c);
-    }
     //Set up boid
-    boid b = boid(&window, texture, &breadcrumbs);
-
+    int numBoids = 1;
+    for (int i = 0; i < numBoids; i++) {
+        SteeringBehavior::boids.push_back(new Boid(&window, texture));
+        sf::Vector2f vector = SteeringBehavior::boids.back()->kinematic.pos;
+        vector.x += Wander::randBinomial();
+        vector.y += Wander::randBinomial();
+        SteeringBehavior::boids.back()->kinematic.pos = vector;
+    }
     std::deque<sf::CircleShape> clickCircles;
 
-    Kinematic character;
-    character.pos = b.getPosition();
-
-    SteeringData steering;
-
+    //Set up steering behaviors.
     Wander wander;
-
+    Separation separation;
+    Flocking flocking;
     Kinematic target;
-    target.pos = character.pos;
+
+
     //Create a timeline with a tic size of 10, updating every 10 ms or 100 fps.
     Timeline global;
     int tic = 10;
     Timeline frameTime(&global, tic);
+
     //CurrentTic starts higher than lastTic so the program starts immediately.
     int64_t currentTic = 0;
     int64_t lastTic = -1;
@@ -165,30 +67,29 @@ int main() {
                 if (event.type == sf::Event::Closed) {
                     window.close();
                 }
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    target.pos = sf::Vector2f(event.mouseButton.x, event.mouseButton.y);
+                    //Update mouse click circles.
+                    std::cout << "X: " << target.pos.x << " Y: " << target.pos.y << std::endl;
+                }
             }
 
-            //Wander steering
-            wander.calculateAcceleration(&steering, character, target);
-
-            //Update character position and orientation.
-            character.update(steering, frameTime.getRealTicLength() * (float)(currentTic - lastTic));
-            b.setRotation(character.orientation * (180.0 / M_PI));
-            //Make the X and Y value go from X = -640.f to 640.f, and Y go from -480.f to 480.f
-            b.setPosition(sf::Vector2f(fmod(character.pos.x, 640.f), fmod(character.pos.y, 480.f)));
-            //Add max value to both X and Y to get rid of negatives, then mod again in case it was positive.
-            b.setPosition(sf::Vector2f(fmod(b.getPosition().x + 640.f, 640.f), fmod(b.getPosition().y + 480.f, 480.f)));
-
-
-
-
+            //Calculate acceleration for all boids.
+            for (Boid *b : SteeringBehavior::boids) {
+                wander.calculateAcceleration(b->steering, b->kinematic, target);
+            }
+            //Update all boids
+            for (Boid *b : SteeringBehavior::boids) {
+                b->update(frameTime.getRealTicLength() * (float)(currentTic - lastTic));
+                if (b->kinematic.id == 0) {
+                    // std::cout << "X Velocity: " << b->kinematic.velocity.x << " Y Velocity: " << b->kinematic.velocity.y << std::endl;
+                }
+            }
             //Draw to window.
             window.clear(sf::Color(0, 128, 128));
-            b.move();
-            for(int i = 0; i < breadcrumbs.size(); i++)
-            {
-                breadcrumbs[i].draw(&window);
+            for (Boid *b : SteeringBehavior::boids) {
+                b->draw();
             }
-            b.draw();
             window.display();
         }
         
--- HW2/P3Alt/SteeringBehavior.cpp
+++ HW2/P3Alt/SteeringBehavior.cpp
@@ -1,30 +1,21 @@
 #include "SteeringBehavior.h"
 
-std::vector<Boid *> SteeringBehavior::boids = std::vector<Boid *>();
+#define RDEC 200 //Free parameter. The radius around the target where we start to decelerate
+#define RSAT 10 //Free parameter. The "good enough" range around the target.
+#define TTTV 1 //Time To Target Velocity. Over what period of time do we want the change in velocity to occur.
+#define MAXVEL 100 //Free parameter?
+#define MAXACCEL 1 //Free parameter?
+
+#define ANGULARDEC 1
+#define MAXROT .8 //Free parameter
+#define ANGULARSAT .6
+
+#define WANDERRATE 5.f
+#define WANDEROFFSET 100.f
+#define WANDERRADIUS 5.f
 
 void PositionMatch::calculateAcceleration(SteeringData *steering, Kinematic character, Kinematic goal) {
-    // sf::Vector2f direction = goal.pos - character.pos;
-    sf::Vector2f direction;
-    //Get the direction for X directly and by wrapping around
-    float directX = goal.pos.x - character.pos.x; 
-    if (directX == 0.f) {
-        direction.x = 0.f;
-    }
-    else {
-        float indirectX = directX - (directX/ fabs(directX)) * 640.f;
-        direction.x = (fabs(directX) < fabs(indirectX)) ? directX : indirectX;
-    }
-    //Get the direction for Y directly and by wrapping around.
-    float directY = goal.pos.y - character.pos.y; 
-    if (directY == 0.f) {
-        direction.y = 0.f;
-    }
-    else {
-        float indirectY = directY - (directY/ fabs(directY)) * 480.f;
-        //Use the smaller (absolute) one.
-        direction.y = (fabs(directY) < fabs(indirectY)) ? directY : indirectY;
-    }
-
+    sf::Vector2f direction = goal.pos - character.pos;
     float distance = findMagnitude(direction);
     float goalSpeed;
     if (distance < RSAT) {
@@ -83,25 +74,21 @@ void RotationMatch::calculateAcceleration(SteeringData *steering, Kinematic char
     steering->angular /= (float)TTTV;
 }
 
-Wander::Wander() {
-}
-
 float Wander::randBinomial() {
+    srand(time(0));
     float first = (float)(rand()) / (float)(RAND_MAX);
     float second = (float)(rand()) / (float)(RAND_MAX);
     return first - second;
 }
 
 void Wander::calculateAcceleration(SteeringData *steering, Kinematic character, Kinematic goal) {
-    //Get how much we want to turn. If it's 1.25 and randBinomial returns 1, then we will turn 1.25 in the circle.
-    steering->wanderOrientation += randBinomial() * WANDER_RATE;
-    //Set our goal orientation at 1.25 from where we are currently facing.
-    goal.orientation = steering->wanderOrientation + character.orientation;
+    //Update wander orientation
+    float wanderOrientation = randBinomial() * WANDERRATE;
 
-    //Find center of circle
-    goal.pos = character.pos + WANDER_OFFSET * sf::Vector2f(cos(character.orientation), sin(character.orientation));
-    //Find target.
-    goal.pos += WANDER_RADIUS * sf::Vector2f(cos(goal.orientation), sin(goal.orientation));
+    goal.orientation = wanderOrientation + character.orientation;
+
+    goal.pos = character.pos + WANDEROFFSET * sf::Vector2f(cos(character.orientation), sin(character.orientation));
+    goal.pos += WANDERRADIUS * sf::Vector2f(cos(goal.orientation), sin(goal.orientation));
 
     OrientationMatch align;
     align.calculateAcceleration(steering, character, goal);
@@ -109,152 +96,4 @@ void Wander::calculateAcceleration(SteeringData *steering, Kinematic character, 
     PositionMatch arrive;
     arrive.calculateAcceleration(steering, character, goal);
 }
-
-void Separation::calculateAcceleration(SteeringData *steering, Kinematic character, Kinematic goal) {
-    for (Boid *b : boids) {
-        if (b->kinematic.id == character.id) {
-            continue;
-        }
-        sf::Vector2f direction;
-        //Get the direction for X directly and by wrapping around
-        float directX = character.pos.x - b->kinematic.pos.x; 
-        if (directX == 0.f) {
-            direction.x = 0.f;
-        }
-        else {
-            float indirectX = directX - (directX/ fabs(directX)) * 640.f;
-            direction.x = (fabs(directX) < fabs(indirectX)) ? directX : indirectX;
-        }
-        //Get the direction for Y directly and by wrapping around.
-        float directY = character.pos.y - b->kinematic.pos.y; 
-        if (directY == 0.f) {
-            direction.y = 0.f;
-        }
-        else {
-            float indirectY = directY - (directY/ fabs(directY)) * 480.f;
-            //Use the smaller (absolute) one.
-            direction.y = (fabs(directY) < fabs(indirectY)) ? directY : indirectY;
-        }
-
-        float distance = findMagnitude(direction);
-
-        if (distance <= THRESHOLD && distance != 0) {
-            float strength = fmin(DECAY_COEFFICIENT / (distance * distance), MAX_ACCEL);
-            steering->linear += strength * normalize(direction);
-        }
-    }
-}
-
-void Flocking::calculateAcceleration(SteeringData *steering, Kinematic character, Kinematic goal) {
-
-    //Begin weighted blending.
-
-    PositionMatch cohesion;
-    VelocityMatch aligner;
-    Separation separator;
-    OrientationMatch orienter;
-
-    //Calculate center of mass of nearby neighbors and average velocity.
-    sf::Vector2f averageVelocity = sf::Vector2f(0.f, 0.f);
-    sf::Vector2f centerOfMass = sf::Vector2f(0.f, 0.f);
-    int numNeighbors = 0;
-    for (Boid *b : boids) {
-        bool invertedX = false;
-        bool invertedY = false;
-        sf::Vector2f direction;
-        //Get the direction for X directly and by wrapping around
-        float directX = b->kinematic.pos.x - character.pos.x; 
-        if (directX == 0.f) {
-            direction.x = 0.f;
-        }
-        else {
-            invertedX = true;
-            float indirectX = directX - (directX/ fabs(directX)) * 640.f;
-            direction.x = (fabs(directX) < fabs(indirectX)) ? directX : indirectX;
-        }
-        //Get the direction for Y directly and by wrapping around.
-        float directY = b->kinematic.pos.y - character.pos.y; 
-        if (directY == 0.f) {
-            direction.y = 0.f;
-        }
-        else {
-            invertedY = true;
-            float indirectY = directY - (directY/ fabs(directY)) * 480.f;
-            //Use the smaller (absolute) one.
-            direction.y = (fabs(directY) < fabs(indirectY)) ? directY : indirectY;
-        }
-
-        float distance = findMagnitude(direction);
-        //NumNeighbors should always end up at one because we are included in the list of boids.
-        if (distance < THRESHOLD) {
-            centerOfMass += character.pos + direction;
-            averageVelocity += b->kinematic.velocity;
-            numNeighbors++;
-        }
-    }
-    centerOfMass /= (float)numNeighbors;
-    averageVelocity /= (float)numNeighbors;
-
-    goal.pos = centerOfMass;
-    goal.velocity = averageVelocity;
-
-    //Do weighted blending for linear acceleration behaviors.
-    SteeringData separationData;
-    separator.calculateAcceleration(&separationData, character, goal); //Separation
-
-    SteeringData alignmentData;
-    aligner.calculateAcceleration(&alignmentData, character, goal); //Velocity matching C.O.M
-
-    SteeringData cohesionData;
-    if (numNeighbors != 1) {
-        cohesion.calculateAcceleration(&cohesionData, character, goal); //Position matching C.O.M
-    }
-
-    separationData.linear *= SEPARATION_WEIGHT;
-    alignmentData.linear *= ALLIGNMENT_WEIGHT;
-    cohesionData.linear *= COHESION_WEIGHT;
-    // std::cout << "Boid " << character.id << " Position: " << character.pos.x << std::endl;
-    // std::cout << "Boid " << character.id << " Separation: " << separationData.linear.x << std::endl;
-    // std::cout << "Boid " << character.id << " Alignment: " << alignmentData.linear.x << std::endl;
-    // std::cout << "Boid " << character.id << " Cohesion: " << cohesionData.linear.x << std::endl;
-    
-    //Find total linear value.
-    steering->linear = SEPARATION_WEIGHT * separationData.linear
-        + ALLIGNMENT_WEIGHT * alignmentData.linear + COHESION_WEIGHT * cohesionData.linear;
-
-    float theta;
-    if ((character.velocity).x != 0) {
-        theta = atan2((character.velocity).y, (character.velocity).x);
-    }
-    else {
-        theta = atan2((character.velocity).y, 0);
-    }
-    goal.orientation = theta;
-
-    //Find total angular value.
-    orienter.calculateAcceleration(steering, character, goal);
-    
-    //Questions:
-    //Should I count in the center of mass/average velocity calculation? 
-    //Should I allign in the direction of steering->linear? 
-    //Should I wander?
-
-    //Answers:
-    //Doesn't matter much, but probably yes. He did mention some situation where you might not though.
-    //Allign in direction of CURRENT velocity, not acceleration
-    //No need to wander.
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
 
